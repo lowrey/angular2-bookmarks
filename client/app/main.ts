c1c144abf55e51d8a62306d7e84da82b43ecf8ee
Your Name <you@example.com>
2016-03-10 22:16:48 -0500

    update angular

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,3 +1,4 @@
+/// <reference path="../../node_modules/angular2/typings/browser.d.ts" />
 import {AppComponent} from "./app.component";
 import {bootstrap}    from "angular2/platform/browser";
 import {provide}    from "angular2/core";
